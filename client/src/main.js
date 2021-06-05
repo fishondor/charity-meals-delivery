96b900ac72f86d252f92db58d8c5cb1546daa315
Ariel Fisher <fishondor@gmail.com>
2021-06-05 23:09:13 +0300

    Replace vue version to 2

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+import firebaseService from './providers/Firebase'
+import { dbService } from './providers/Db'
+import notificationsService from './providers/Notifications'
+import logger from './providers/Logger'
+import loaderService from './providers/Loader'
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    store,
+    vuetify,
+    firebaseService,
+    dbService,
+    notificationsService,
+    logger,
+    loaderService,
+    created: function(){
+      firebaseService.init(store, logger);
+      notificationsService.init(store);
+      loaderService.init(store);
+    },
+    render: h => h(App)
+  }).$mount('#app')
