48574c4809ad9cbd506124b497dcb240465d7c9a
Ariel Fisher <fishondor@gmail.com>
2021-06-23 21:43:22 +0300

    Update delivery component with nested routes

--- client/src/views/Delivery.vue
+++ client/src/views/Delivery.vue
@@ -1,32 +1,66 @@
 <template>
     <v-container>
-        <h3>{{delivery.date | formatDate}}</h3>
+        <h3>{{date | formatDate}}</h3>
+        <router-view></router-view>
     </v-container>
 </template>
 
 <script>
+
 export default {
     data: () => ({
-        delivery: {}
+        deliveryref: null,
+        date: null,
+        groups: [],
+        isAdmin: false,
+        isRegistered: false,
+        deliveryId: null
     }),
-    mounted(){
-        this.getDelivery()
+    async mounted(){
+        this.deliveryId = this.$route.params.id
+        //this.isAdmin = await this.isDeliveryAdmin(this.deliveryId)
+        this.isRegistered = await this.isRegisteredToDelivery()
+        console.log("Is admin", this.isAdmin)
+        this.getDelivery(this.deliveryId)
     },
     methods: {
-        getDelivery: async function(){
-            let deliveryId = this.$route.params.id
-            console.log("D", deliveryId)
+        isDeliveryAdmin: async function(deliveryId){
+            let adminId = await this.getDeliveryAdmin(deliveryId)
+            let user = await this.$firebaseService.getCurrentUser()
+            return user.uid == adminId
+        },
+        getDeliveryAdmin: async function(deliveryId){
             try{
-                let result = await this.$dbService.getDelivery(deliveryId)
+                let result = await this.$firebaseService.getDeliveryOwner(deliveryId)
                 if(!result){
                     this.$notificationsService.error("An error occured");
-                    return
+                    return false
                 }
-                this.delivery = result
+                return result
             }catch(error){
                 console.error("Error fetching delivery", error)
                 this.$notificationsService.error("An error occured");
             }
+            return false
+        },
+        getDelivery: async function(deliveryId){
+            try{
+                let result = await this.$firebaseService.getDelivery(deliveryId)
+                if(!result){
+                    this.$notificationsService.error("An error occured");
+                    return
+                }
+                this.deliveryref = result;
+                this.deliveryref.once('value', (snapshot) => {
+                    this.date = snapshot.child('date').val()
+                });
+            }catch(error){
+                console.error("Error fetching delivery", error)
+                this.$notificationsService.error("An error occured");
+            }
+        },
+        isRegisteredToDelivery: async function(){
+            return false
         }
     }
 }
