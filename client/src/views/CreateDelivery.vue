b2724cd069e471a9b65678df55fad86ba57b3701
Ariel Fisher <fishondor@gmail.com>
2021-06-23 21:42:34 +0300

    Use firebase as vue service

--- client/src/views/CreateDelivery.vue
+++ client/src/views/CreateDelivery.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-import firebaseService from '../providers/Firebase'
 import FormDelivery from '../components/FormDelivery'
 
 export default {
@@ -16,15 +15,15 @@ export default {
     methods: {
         submit: async function(delivery){
             try{
-                let owner = await firebaseService.getCurrentUser();
+                let owner = await this.$firebaseService.getCurrentUser();
                 delivery.owner = owner.uid
-                firebaseService.createDelivery(delivery)
-                /* let result = await this.$dbService.saveDelivery(content);
-                if(!result){
+                let deliveryId = await this.$firebaseService.createDelivery(delivery)
+                if(!deliveryId){
                     this.$notificationsService.error("An error occured while saving");
                     return
                 }
-                this.$notificationsService.success("Saved"); */
+                this.$notificationsService.success("Saved");
+                this.$router.push({name: 'Delivery', params: { id: deliveryId }})
             }catch(err){
                 this.$notificationsService.error("Could not save. Please try again later");
                 console.error(err)
