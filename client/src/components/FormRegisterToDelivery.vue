dcf46cb9633ab3eb78b7bd1a58f0273b7e10ce4b
Ariel Fisher <fishondor@gmail.com>
2021-06-23 21:40:47 +0300

    Add components

---
+++ client/src/components/FormRegisterToDelivery.vue
@@ -0,0 +1,105 @@
+<template>
+    <v-form
+        ref="form"
+        v-model="formValid"
+    >
+        <v-container>
+            <v-row>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                    <v-text-field
+                        v-model="content.name"
+                        label="Name"
+                        :disabled="!!user.displayName"
+                        required
+                    ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                    <v-text-field
+                        v-model="content.email"
+                        label="Email"
+                        :disabled="!!user.email"
+                        required
+                    ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                    <vue-tel-input-vuetify 
+                        v-model="content.phone"
+                        defaultCountry="IL"
+                        :onlyCountries="['IL']"
+                        :disabled="!!user.phoneNumber"
+                        required ></vue-tel-input-vuetify>
+                </v-col>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                    <v-text-field
+                        v-model="content.pickupsNumber"
+                        label="How many pickups?"
+                        type="number"
+                        required
+                    ></v-text-field>
+                </v-col>
+                <v-col
+                    cols="12"
+                    md="6"
+                >
+                    <v-btn
+                        color="primary"
+                        class="mr-4"
+                        @click="submit"
+                        >
+                        Register
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+import Carrier from '../models/Carrier'
+
+export default {
+    data: () => ({
+        content: {
+            name: "",
+            phone: "",
+            pickupsNumber: 1,
+            email: ""
+        },
+        user: {},
+        formValid: false
+    }),
+    async mounted(){
+        this.user = await this.$firebaseService.getCurrentUser();
+        this.content.name = this.user.displayName;
+        this.content.email = this.user.email;
+        this.content.phone = this.user.phoneNumber;
+        console.log("User", this.user)
+    },
+    methods: {
+        submit: function(){
+            let carrier = new Carrier(
+                {
+                    email: this.content.email,
+                    name: this.content.name,
+                    phone: this.content.phone,
+                    pickupsNumber: this.content.pickupsNumber,
+                    uid: this.user.uid
+                }
+            )
+            this.$emit('onSubmit', carrier);
+        }
+    }
+}
+</script>
---
+++ client/src/components/FromGroup.vue
@@ -0,0 +1,20 @@
+<template>
+    <vue-repeater v-model="fields"></vue-repeater>
+</template>
+<script>
+import VueRepeater from 'vue-repeater'
+import 'vue-repeater/dist/vue-repeater.css'
+
+export default {
+    data () {
+        return {
+            fields: [
+                {
+                    name: 'FormPickup',
+                    value: {}
+                }
+            ]
+        }
+    }
+}
+</script>
