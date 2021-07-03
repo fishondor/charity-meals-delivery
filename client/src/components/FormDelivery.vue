89a90913227a053787f7877e00c11f4f5d8027bd
Ariel Fisher <fishondor@gmail.com>
2021-07-03 16:58:14 +0300

    Change button color

--- client/src/components/FormDelivery.vue
+++ client/src/components/FormDelivery.vue
@@ -11,7 +11,7 @@
             </v-card-text>
             <v-card-actions>
                 <v-btn
-                    color="success"
+                    color="primary"
                     class="mr-4"
                     @click="submit"
                     >
