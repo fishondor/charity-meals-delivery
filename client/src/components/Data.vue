2514b6fe43fc12ab1b6031095e8882ec7b9fc12e
Ariel Fisher <fishondor@gmail.com>
2021-07-11 01:00:05 +0300

    Enable reseting groups

--- client/src/components/Data.vue
+++ client/src/components/Data.vue
@@ -44,6 +44,30 @@
                     <v-list-item-title>מספר מחלקים חסר: {{groups.length - carriersDelivaryCount}}</v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
+            <v-list-item>
+                <v-list-item-icon>
+                    <v-btn icon @click="resetGroupsClicked">
+                        <v-icon 
+                            v-text="icons.deleteAlert"
+                            color="warning"
+                        ></v-icon>
+                    </v-btn>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title>מחק את כל החלוקות</v-list-item-title>
+                </v-list-item-content>
+                <v-dialog v-model="dialogDelete">
+                    <v-card>
+                        <v-card-title class="text-h5">פעולה זו תמחוק את כל החלוקות הקיימות במערכת. האם למחוק?</v-card-title>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="closeDelete">בטל</v-btn>
+                            <v-btn color="blue darken-1" text @click="deleteGroupsConfirm">מחק</v-btn>
+                            <v-spacer></v-spacer>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-list-item>
         </v-list>
     </v-container>
 </template>
@@ -53,7 +77,8 @@ import Carrier from '../models/Carrier'
 
 import {
     copy,
-    whatsapp
+    whatsapp,
+    deleteAlert
 } from './icons'
 
 export default {
@@ -65,16 +90,19 @@ export default {
             carriersDelivaryCount: 0,
             icons: {
                 copy,
-                whatsapp
+                whatsapp,
+                deleteAlert
             },
             deliveryDate: '',
-            deliveryDescription: ''
+            deliveryDescription: '',
+            dialogDelete: false,
+            deliveryRef: null
         }
     },
     async created(){
         this.deliveryId = this.$route.params.id
-        let deliveryRef = await this.$firebaseService.getDelivery(this.deliveryId)
-        deliveryRef.on('value', this.setData);
+        this.deliveryRef = await this.$firebaseService.getDelivery(this.deliveryId)
+        this.deliveryRef.on('value', this.setData);
     },
     computed: {
         shareText() {
@@ -105,7 +133,27 @@ export default {
             document.execCommand("copy");
             document.body.removeChild(tempInput);
             this.$notificationsService.success("Cpied to clipboard")
-        }
-    }
+        },
+        deleteGroupsConfirm () {
+            try{
+                this.deliveryRef.child('groups').remove()
+                this.closeDelete()
+                this.$notificationsService.success("Successfully deleted all groups")
+            }catch(error){
+                this.$notificationsService.error("Error deleting all groups")
+            }
+        },
+        closeDelete () {
+            this.dialogDelete = false
+        },
+        resetGroupsClicked () {
+            this.dialogDelete = true
+        },
+    },
+    watch: {
+        dialogDelete (val) {
+            val || this.closeDelete()
+        },
+    },
 }
 </script>
