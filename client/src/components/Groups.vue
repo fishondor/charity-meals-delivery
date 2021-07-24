c0b65154d4c7cbc6992009a7d6f5e8f2cbb7213c
Ariel Fisher <fishondor@gmail.com>
2021-07-25 00:09:23 +0300

    Enable creating new group from admin

--- client/src/components/Groups.vue
+++ client/src/components/Groups.vue
@@ -1,5 +1,30 @@
 <template>
     <v-container>
+        <v-dialog
+            v-model="dialog"
+            max-width="500px"
+        >
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    v-bind="attrs"
+                    v-on="on"
+                    >
+                    הוסף חלוקה
+                </v-btn>
+            </template>
+            <v-card>
+                <v-card-title>
+                    <span class="text-h5">חלוקה</span>
+                </v-card-title>
+
+                <v-card-text>
+                    <FormGroup :indexes="groupsIndexes" @onSubmit="createSingleDelivery" />
+                </v-card-text>
+            </v-card>
+        </v-dialog>
         <template v-if="groups.length">
             <TablePickups v-for="group in sortedGroups" :key="group.index"
                 :group="group"
@@ -29,13 +54,15 @@
 </template>
 <script>
 import TablePickups from './TablePickups'
+import FormGroup from './FormGroup'
 
 import Group from '../models/Group'
 import Carrier from '../models/Carrier'
 
 export default {
     components: {
-        TablePickups
+        TablePickups,
+        FormGroup
     },
     data: () => ({
         groups: [],
@@ -43,7 +70,8 @@ export default {
         exportedData: "",
         deliveryId: null,
         groupsRef: null,
-        carriersRef: null
+        carriersRef: null,
+        dialog: false,
     }),
     created(){
         this.deliveryId = this.$route.params.id
@@ -92,6 +120,16 @@ export default {
                 }
             )
         },
+
+        closeSingleDeliveryDialog () {
+            this.dialog = false
+        },
+
+        createSingleDelivery (data) {
+            let group = new Group(data)
+            this.groupsRef.push(group)
+            this.closeSingleDeliveryDialog()
+        },
     },
     computed: {
         sortedGroups() {
@@ -102,7 +140,17 @@ export default {
                     return a.index - b.index
                 }
             )
+        },
+        groupsIndexes() {
+            return this.groups.map(
+                item => item.index
+            )
         }
-    }
+    },
+    watch: {
+        dialog (val) {
+            val || this.closeSingleDeliveryDialog()
+        },
+    },
 }
 </script>
