9477d3bc1e60ad89b80fbd13ae2f4f42df9ce21e
Ariel Fisher <fishondor@gmail.com>
2021-07-24 23:49:36 +0300

    Update Hebrew text

--- client/src/components/TablePickups.vue
+++ client/src/components/TablePickups.vue
@@ -115,11 +115,11 @@
         </v-dialog>
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
-            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-title class="text-h5">האם למחוק איסוף זה?</v-card-title>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-btn color="blue darken-1" text @click="closeDelete">ביטול</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">מחק</v-btn>
               <v-spacer></v-spacer>
             </v-card-actions>
           </v-card>
