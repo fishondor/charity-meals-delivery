48e5ad090fd8c554a4584395ef647902a3b37745
Ariel Fisher <fishondor@gmail.com>
2021-07-31 22:40:09 +0300

    Enable running script with arguments

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,4 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASS" \
---output $GITHUB_WORKSPACE/client/cypress.env.json $GITHUB_WORKSPACE/client/cypress.env.json.gpg
+--output $GITHUB_WORKSPACE/client/$1.json $GITHUB_WORKSPACE/client/$1.json.gpg
