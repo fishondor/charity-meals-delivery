eea2c838179d63073a4e088b000f7b5c1507e9b2
Ariel Fisher <fishondor@gmail.com>
2021-08-03 23:53:59 +0300

    Use proccess.env directly in Environment

--- client/src/providers/Environment.js
+++ client/src/providers/Environment.js
@@ -1,20 +1,12 @@
 import logger from './Logger'
 
-const constants = {
-    VUE_APP_FIREBASE_API_KEY: process.env.VUE_APP_FIREBASE_API_KEY,
-    VUE_APP_FIREBASE_AUTH_DOMAIN: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-    VUE_APP_FIREBASE_DATABASE_URL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-    VUE_APP_FIREBASE_PROJECT_ID: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-    VUE_APP_FIREBASE_STORAGE_BUCKET: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-}
-
 const get = (name) => {
-    if(!constants[name]){
+    if(!process.env[name]){
         logger.error(`could not find ${name}`)
         return null
     }
 
-    return constants[name]
+    return process.env[name]
 }
 
 export default {
