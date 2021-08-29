1156dc57f1fca9ac2ca158d00780b809c47a7eb4
Ariel Fisher <fishondor@gmail.com>
2021-08-30 00:02:01 +0300

    Add deliveries and destinations admin tables

---
+++ client/src/components/TableDeliveries.vue
@@ -0,0 +1,117 @@
+<template>
+    <v-container id="deliveries-table">
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                        <th>
+                            מזהה
+                        </th>
+                        <th>
+                            תאריך
+                        </th>
+                        <th>
+                            הערות
+                        </th>
+                        <th>
+                            קישור
+                        </th>
+                        <th>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        v-for="item in deliveries"
+                        :key="item.id"
+                    >
+                        <td>{{ item.id }}</td>
+                        <td>{{ item.date }}</td>
+                        <td>{{ item.description }}<td>
+                        <td><a :href="item.id | deliveryLink">{{ item.id | deliveryLink }}</a></td>
+                        <td>
+                            <v-icon
+                                @click="deleteItem(item)"
+                                class="deliveries-delete-item"
+                            >
+                                {{icons.deleteIcon}}
+                            </v-icon>
+                        </td>
+                    </tr>
+                </tbody>
+            </template>
+        </v-simple-table>
+        <v-card-actions>
+            <v-btn
+                color="primary"
+                to="/delivery/new"
+                >
+                חדש
+            </v-btn>
+        </v-card-actions>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn id="delete-delivery-dialog-approve" color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+    </v-container>
+</template>
+<script>
+import Delivery from '../models/Delivery'
+import {
+    deleteIcon
+} from './icons'
+
+export default {
+    data: () => ({
+        deliveries: [],
+        icons: {
+            deleteIcon
+        },
+        dialogDelete: false,
+    }),
+    async created(){
+        let user = await this.$firebaseService.getCurrentUser()
+        let deliveriesSnapshot = await this.$firebaseService.getDeliveriesByOwner(user.uid)
+        this.deliveries = Delivery.fromSnapshot(deliveriesSnapshot)
+    },
+    watch: {
+        dialog (val) {
+            val || this.close()
+        },
+        dialogDelete (val) {
+            val || this.closeDelete()
+        },
+    },
+    methods: {
+        deleteItem (item) {
+            this.idToDelete = item.id
+            this.dialogDelete = true
+        },
+
+        deleteItemConfirm () {
+            let response = this.$firebaseService.deleteDelivery(this.idToDelete)
+            if(!response){
+                this.$notificationsService.error("Could not delete delivery")
+                return
+            }
+            this.deliveries = this.deliveries.filter(
+                item => item.id != this.idToDelete
+            )
+            this.idToDelete = null
+            this.closeDelete()
+            this.$notificationsService.success(`Delivery ${this.idToDelete} deleted`)
+        },
+
+        closeDelete () {
+            this.dialogDelete = false
+        },
+    }
+}
+</script>
---
+++ client/src/components/TableDestinations.vue
@@ -0,0 +1,130 @@
+<template>
+    <v-container id="deliveries-table">
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                        <th>
+                            מזהה
+                        </th>
+                        <th>
+                            שם
+                        </th>
+                        <th>
+                            טלפון
+                        </th>
+                        <th>
+                            כתובת
+                        </th>
+                        <th>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        v-for="item in destinations"
+                        :key="item.id"
+                    >
+                        <td>{{ item.id }}</td>
+                        <td>{{ item.name }}</td>
+                        <td>{{ item.phone }}</td>
+                        <td>{{ item.address }}</td>
+                        <td>
+                            <v-icon
+                                @click="deleteItem(item)"
+                                class="destinations-delete-item"
+                            >
+                                {{icons.deleteIcon}}
+                            </v-icon>
+                        </td>
+                    </tr>
+                </tbody>
+            </template>
+        </v-simple-table>
+        <v-card-actions>
+            <DialogDestination @onCreate='onCreateDestination' />
+        </v-card-actions>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn id="delete-destination-dialog-approve" color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+    </v-container>
+</template>
+<script>
+import DialogDestination from './DialogDestination.vue'
+
+import Destination from '../models/Destination'
+import {
+    deleteIcon
+} from './icons'
+
+export default {
+    components: {
+        DialogDestination
+    },
+    data: () => ({
+        destinations: [],
+        icons: {
+            deleteIcon
+        },
+        dialogDelete: false
+    }),
+    async created(){
+        let user = await this.$firebaseService.getCurrentUser()
+        let destinationsSnapshot = await this.$firebaseService.getDestinationsByOwner(user.uid)
+        this.destinations = Destination.fromSnapshot(destinationsSnapshot)
+    },
+    watch: {
+        dialog (val) {
+            val || this.close()
+        },
+        dialogDelete (val) {
+            val || this.closeDelete()
+        },
+    },
+    methods: {
+        deleteItem (item) {
+            this.idToDelete = item.id
+            this.dialogDelete = true
+        },
+
+        async deleteItemConfirm () {
+            let user = await this.$firebaseService.getCurrentUser()
+            let response = this.$firebaseService.deleteDestination(user.uid, this.idToDelete)
+            if(!response){
+                this.$notificationsService.error("Could not delete destination")
+                return
+            }
+            this.destinations = this.destinations.filter(
+                item => item.id != this.idToDelete
+            )
+            this.$notificationsService.success(`Destination ${this.idToDelete} deleted`)
+            this.idToDelete = null
+            this.closeDelete()
+        },
+
+        closeDelete () {
+            this.dialogDelete = false
+        },
+
+        async onCreateDestination (destination) {
+            let user = await this.$firebaseService.getCurrentUser()
+            let destinationId = await this.$firebaseService.saveDestination(user.uid, destination)
+            if(!destinationId){
+                this.$notificationsService.error(`Could not save destination. Please try again later`)
+                return
+            }
+            this.$notificationsService.success(`Destination ${destinationId} saved successfully`)
+            destination.id = destinationId
+            this.destinations.push(destination)
+        }
+    }
+}
+</script>
