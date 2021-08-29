dfb01997bfa4a61e51e9360a6b31dadaed27fb28
Ariel Fisher <fishondor@gmail.com>
2021-08-30 00:06:53 +0300

    Add destination test

--- e2e/tests/RegisterUserToPickup.js
+++ e2e/tests/RegisterUserToPickup.js
@@ -57,15 +57,34 @@ module.exports = {
     "Set carrier for this group": async (browser) => {
         let groupCarrierSelector = `.table-pickups:nth-child(${this.randomGroupNumber + 1}) .group-carrier`
         browser.waitForElementVisible(groupCarrierSelector)
-        await browser.moveToElement(`.table-pickups:nth-child(${this.randomGroupNumber + 1}) div:last-child`, 0, 0)
+        browser.moveToElement(`.table-pickups:nth-child(${this.randomGroupNumber + 1}) div:last-child`, 0, 0)
+        browser.pause(3000)
         browser.click(groupCarrierSelector)
         browser.waitForElementVisible('xpath', `//div[text()='${this.carrier.name}']`)
         browser.click('xpath', `//div[text()='${this.carrier.name}']`)
+    },
+
+    "Set destination for this group": async (browser) => {
+        browser.waitForElementNotPresent('.v-autocomplete__content')
+        let groupDestinationSelector = `.table-pickups:nth-child(${this.randomGroupNumber + 1}) .group-destination-select`
+        browser.waitForElementVisible(groupDestinationSelector)
+        await browser.moveToElement(`.table-pickups:nth-child(${this.randomGroupNumber + 1}) div:last-child`, 0, 0)
+        browser.click(groupDestinationSelector)
+        browser.waitForElementVisible('.v-autocomplete__content')
+
+        let destinationsOptions = await browser.elements('css selector', '.v-autocomplete__content .v-list-item')
+        let randomDestinationNumber = Math.floor(Math.random() * destinationsOptions.value.length)
+        let randomDestinationOptionSelector = `.v-autocomplete__content .v-list-item:nth-child(${randomDestinationNumber + 1}) .v-list-item__title`
+        let randomDestinationOption = await browser.elements('css selector', randomDestinationOptionSelector)
+        this.groupDestinationName = await browser.elementIdAttribute(randomDestinationOption.value[0].ELEMENT, 'innerText')
+        console.log("Selected destination", this.groupDestinationName)
+
+        browser.click(randomDestinationOptionSelector)
 
         browser.Logout()
     },
 
-    "Log in as carrier 2nd time": async (browser) => {
+    "2nd time Log in as carrier": async (browser) => {
         browser.url(this.url)
         browser.waitForElementVisible('body')
         await browser.Login(browser.globals.EXAMPLE_DELIVERY_WITH_GROUPS.CARRIER_INDEX)
@@ -74,6 +93,11 @@ module.exports = {
     "Selected delivery groups": (browser) => {
         browser.waitForElementVisible(".table-pickups .group-description")
         browser.assert.containsText(".table-pickups .group-description", this.groupDescription.value);
+    },
+
+    "Delivery has correct destination": (browser) => {
+        browser.waitForElementVisible(".table-destination")
+        browser.assert.containsText(".table-destination .destination-name", this.groupDestinationName.value)
     }
 
 }
