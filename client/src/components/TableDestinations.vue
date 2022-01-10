95945cbec9736fb730209e66a58f4cbf84522f8f
Ariel Fisher <fishondor@gmail.com>
2022-01-10 22:56:25 +0200

    Set table destination to work with cooks

--- client/src/components/TableDestinations.vue
+++ client/src/components/TableDestinations.vue
@@ -5,7 +5,10 @@
                 <thead>
                     <tr>
                         <th>
-                            מזהה
+                            מספר
+                        </th>
+                        <th>
+                            תיאור
                         </th>
                         <th>
                             שם
@@ -18,6 +21,8 @@
                         </th>
                         <th>
                         </th>
+                        <th>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -25,10 +30,19 @@
                         v-for="item in destinations"
                         :key="item.id"
                     >
-                        <td>{{ item.id }}</td>
+                        <td>{{ item.index }}</td>
+                        <td>{{ item.description }}</td>
                         <td class="destinations-table-item-name">{{ item.name }}</td>
                         <td>{{ item.phone }}</td>
                         <td>{{ item.address }}</td>
+                        <td>
+                            <v-icon
+                                @click="editItem(item)"
+                                class="destinations-edit-item"
+                            >
+                                {{icons.edit}}
+                            </v-icon>
+                        </td>
                         <td>
                             <v-icon
                                 @click="deleteItem(item)"
@@ -42,7 +56,7 @@
             </template>
         </v-simple-table>
         <v-card-actions>
-            <DialogDestination @onCreate='onCreateDestination' />
+            <DialogDestination @onCreate='onCreateDestination'/>
         </v-card-actions>
         <v-dialog content-class="dialog-delete-destination" v-model="dialogDelete" max-width="500px">
           <v-card>
@@ -55,29 +69,59 @@
             </v-card-actions>
           </v-card>
         </v-dialog>
+        <v-dialog content-class="dialog-edit-destination" v-model="dialogEdit">
+            <v-card>
+                <v-card-title>
+                    <span class="text-h5">יעד לחלוקה</span>
+                </v-card-title>
+                <v-card-text>
+                    <FormDestination
+                        :content="destinationItem"/>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="dialogEdit = false">ביטול</v-btn>
+                    <v-btn id="edit-destination-dialog-approve" color="blue darken-1" text @click="editItemConfirm(destinationItem)">שמור</v-btn>
+                    <v-spacer></v-spacer>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-container>
 </template>
 <script>
+import FormDestination from './FormDestination.vue'
 import DialogDestination from './DialogDestination.vue'
 
 import Destination from '../models/Destination'
 import {
-    deleteIcon
+    deleteIcon,
+    edit
 } from './icons'
 
 export default {
     components: {
-        DialogDestination
+        DialogDestination,
+        FormDestination
     },
     data: () => ({
         destinations: [],
         icons: {
-            deleteIcon
+            deleteIcon,
+            edit
         },
-        dialogDelete: false
+        dialogDelete: false,
+        dialogEdit: false,
+        itemToEdit: {},
+        destinationItem: {},
+        destinationsRef: null
     }),
     async created(){
-        this.setDestinations()
+        let user = await this.$firebaseService.getCurrentUser()
+        this.destinationsRef = this.$firebaseService.getDestinationsRef(user.uid)
+        this.destinationsRef.on('value', (snapshot) => {
+            this.destinations = Destination.fromSnapshot(snapshot)
+        });
+        this.validateIndexes(this.destinations)
     },
     watch: {
         dialog (val) {
@@ -87,16 +131,38 @@ export default {
             val || this.closeDelete()
         },
     },
+    computed: {
+        destinationsIndexes() {
+            return this.destinations.map(
+                item => item.index
+            )
+        }
+    },
     methods: {
-        async setDestinations () {
-            let user = await this.$firebaseService.getCurrentUser()
-            let destinationsSnapshot = await this.$firebaseService.getDestinationsByOwner(user.uid)
-            this.destinations = Destination.fromSnapshot(destinationsSnapshot)
+        async validateIndexes (destinations) {
+            destinations.map(
+                async (destination, index) => {
+                    if(!destination.index || destination.index != index + 1){
+                        destination.index = index + 1
+                        await this.editItemConfirm(destination)
+                    }
+                }
+            )
         },
 
         deleteItem (item) {
             this.idToDelete = item.id
             this.dialogDelete = true
+        },
+
+        editItem(item) {
+            this.destinationItem = item;
+            this.dialogEdit = true
+        },
+
+        closeEdit() {
+            this.destinationItem = {}
+            this.dialogEdit = false
         },
 
         async deleteItemConfirm () {
@@ -106,9 +172,9 @@ export default {
                 this.$notificationsService.error("Could not delete destination")
                 return
             }
-            this.setDestinations()
             this.$notificationsService.success(`Destination ${this.idToDelete} deleted`)
             this.idToDelete = null
+            this.validateIndexes(this.destinations)
             this.closeDelete()
         },
 
@@ -118,13 +184,24 @@ export default {
 
         async onCreateDestination (destination) {
             let user = await this.$firebaseService.getCurrentUser()
+            destination.index = this.destinations.length + 1
             let destinationId = await this.$firebaseService.saveDestination(user.uid, destination)
             if(!destinationId){
                 this.$notificationsService.error(`Could not save destination. Please try again later`)
                 return
             }
-            this.setDestinations()
-            this.$notificationsService.success(`Destination ${destinationId} saved successfully`)
+            this.$notificationsService.success(`Destination created successfully`)
+        },
+
+        async editItemConfirm(destination){
+            let user = await this.$firebaseService.getCurrentUser()
+            let destinationId = await this.$firebaseService.editDestination(user.uid, destination)
+            if(!destinationId){
+                this.$notificationsService.error(`Could not edit destination. Please try again later`)
+                return
+            }
+            this.$notificationsService.success(`Destination updated successfully`)
+            this.closeEdit()
         }
     }
 }
