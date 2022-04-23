9f1b26acd4c168e280fe2c994f057e4bfa4f9e22
fishondor <fishondor@gmail.com>
2022-04-23 23:40:23 +0300

    Remove set timeOptions for delivery

--- client/src/components/FormDelivery.vue
+++ client/src/components/FormDelivery.vue
@@ -16,36 +16,6 @@
                     outlined
                 >
                 </v-text-field>
-                <v-row>
-                    <v-col
-                        cols="12"
-                        md="6"
-                    >
-                        <v-select
-                            v-model="content.timeOptions"
-                            :items="timeOptions"
-                            chips
-                            label="אפשרויות לשעות חלוקה"
-                            multiple
-                            dense
-                            outlined
-                            item-text="time"
-                            return-object
-                        ></v-select>
-                    </v-col>
-                    <v-col
-                        cols="12"
-                        md="6"
-                    >
-                        <v-text-field v-for="(item, index) in content.timeOptions" :key="index"
-                            v-model="item.numOfCarriers"
-                            :label="`כמה מחלקים דרושים לשעה ${item.time}? (0 לא יגביל מספר נרשמים)`"
-                            type="number"
-                            :min="0"
-                            :max="30"
-                        ></v-text-field>
-                    </v-col>
-                </v-row>
             </v-card-text>
             <v-card-actions>
                 <v-btn
@@ -70,16 +40,14 @@ export default {
             date: new Date().toISOString().substr(0, 10),
             description: ""
         },
-        formValid: false,
-        timeOptions: Delivery.TIME_OPTIONS,
+        formValid: false
     }),
     methods: {
         submit: function(){
             let delivery = new Delivery(
                 {
                     date: this.content.date,
-                    description: this.content.description,
-                    timeOptions: this.content.timeOptions
+                    description: this.content.description
                 }
             )
             this.$emit('onSubmit', delivery);
